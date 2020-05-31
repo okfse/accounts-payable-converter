5852544bc301ed24e58350bb96fdb133c72dfe9a
Pierre Mesure <pierre@mesu.re>
2020-05-31 16:51:47 +0200

    Add script to convert files from Umeå Kommun

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# account-payables-converter
- 
+# Accounts Payable Converter
+
+This is a repository for scripts to convert various formats for accounts payable to the Swedish standard that we are using for the project Open Up!.
+
+The script is making some arbitrary choices such as:
+
+* assigning the field *datum* to the first day of the month when only the month is given
---
+++ umea.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+def convert(filename):
+  data = pd.read_csv(
+                  filename + '.csv',
+                  sep=';',
+                  names=['datum', 'forvaltning', 'leverantor', 'leverantor_id', 'konto_nr', 'konto_text', 'belopp', 'faktura_nr'],
+                  header=0
+                  )
+
+  data = data[['faktura_nr', 'leverantor', 'leverantor_id', 'forvaltning', 'konto_nr', 'konto_text', 'belopp', 'datum']]
+
+  data.insert(0, 'kopare', 2120002627)
+  data.insert(1, 'kopare_text', 'Umeå Kommun')
+  data.insert(10, 'grund', '')
+  data.insert(11, 'avtal', '')
+  data.insert(12, 'kommun_id', 2480)
+
+  data.datum = pd.to_datetime(data.datum, format='%Y%m')
+
+  data.leverantor_id = data.leverantor_id.apply(lambda x: '' if '*' in x else x)
+  data.leverantor_id = data.leverantor_id.str.replace('-', '')
+  data.leverantor = data.leverantor.astype(str)
+  data.leverantor = data.leverantor.apply(lambda x: 'ANONYMERAD' if '*' in x else x)
+
+  data.to_csv(filename + "-formatted.csv", index=False)
+
+#convert("leverantorsfakturor-2017")
+#convert("leverantorsfakturor-2018")
+#convert("leverantorsfakturor-2019")
+convert("leverantorsfakturor-2020")
